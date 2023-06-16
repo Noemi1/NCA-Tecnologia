c57ea45d2bd509a78115bb89ec2bbf0d516f3650
Noemi1 <calmeida.no@gmail.com>
2023-06-16 16:34:13 -0300

    incluindo novas seções

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,6 @@ $(document).ready(function () {
     function setScreenSize() {
         $(window).scrollLeft(0)
         let size = $(window).width();
-        console.log(size)
         if (size < 576) {
             screenSize = 'SM'
         }
@@ -39,63 +38,34 @@ $(document).ready(function () {
     }
     $(window).scrollLeft(0);
 
-    $('.atuacao-slider').slick({
+    $('.sobre-slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        fade: true,
+        asNavFor: '.sobre-slider-nav'
+    });
+    $('.sobre-slider-for-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        fade: true,
+        asNavFor: '.sobre-slider-nav'
+    });
+
+
+    $('.sobre-slider-nav').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        infinite: false,
+        asNavFor: '.sobre-slider-for-text, .sobre-slider-for',
         dots: false,
-        arrows: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        draggable: true,
-        adaptiveHeight: false,
-        variableWidth: true,
-        centerMode: false,
-    });
-    // $('.header-navigation-link').unbind('mouseenter');
-    // $('.header-navigation-link').unbind('mouseleave');
-
-    $('.header-navigation-link').on('mouseenter', function () {
-        console.log(screenSize)
-        if (screenSize == 'LG' || screenSize == 'XL') {
-            let left = $(this).position().left;
-            let width = $(this).innerWidth();
-            $('.header-navigation-link').removeClass('hover');
-            $(this).addClass('hover')
-            $('.border-movement').animate({
-                left: left + 'px',
-                width: width + 'px'
-            }, 200)
-        }
+        centerMode: true,
+        focusOnSelect: true
     });
 
-    $('.header-navigation-link').on('mouseleave', function () {
-        if (screenSize == 'LG' || screenSize == 'XL') {
-            $('.header-navigation-link').removeClass('hover');
-            setTimeout(function () {
-                if ($('.header-navigation-link.hover').length == 0) {
-                    $('.border-movement').animate({
-                        left: 0 + 'px',
-                        width: 0 + 'px'
-                    }, 300);
-                }
-            }, 100);
-
-        }
-    });
 });
-
-function setHeaderScroll() {
-    if (window.navigator.brave) {
-        $('.header').addClass('scroll')
-    } else {
-        if ($(window).scrollTop() > 10) {
-            $('.header').addClass('scroll')
-        } else {
-            $('.header').removeClass('scroll')
-        }
-    }
-}
 
 
 function toggleHeaderNavigation() {
